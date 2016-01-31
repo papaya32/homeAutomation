a69120f9e6d8f69e04b39fb6b3b821b6619cc574
papaya96 <oreillyj@purdue.edu>
2016-01-31 00:43:07 -0500

    changed button press checks
    interrupts were very problematic with servo library; compiles cleanly

--- arduino/door_lock.ino
+++ arduino/door_lock.ino
@@ -20,28 +20,29 @@ bool lockState = LOW;
 int servoPin = 14;
 int ledPin = 16;
 int doorSensor = 15;
-int buttonUnlock = 12;
-int buttonLock = 13;
-int buttonWait = 5;
-int accessRelay = 2;
+
+#define buttonUnlock 12
+#define buttonLock 13
+#define buttonWait 4
+#define accessRelay 2
+
+bool currentStateButton = LOW;
+bool lastStateButton = LOW;
+
+#define numButtons 4
+char* buttonArray[numButtons] = {"12", "13", "5", "2"};
 
 int lockDegree = 150;
-unsigned long referenceTime = 300;
-unsigned long lastTime = 0;
+int counter = 0;
+unsigned long referenceTime = 210;
 
 Servo lockServo;
 
 void lockDoor(int);
-void accessVerify(unsigned long);
 void dimmer();
 void setup_wifi();
 void callback(char*, byte*, unsigned int);
-
-//Interrupt function initializations
-void lock();
-void unlock();
-void wait();
-void access();
+void buttonPress();
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -52,18 +53,11 @@ int value = 0;
 void setup()
 {
   pinMode(ledPin, OUTPUT);
-  pinMode(buttonLock, OUTPUT);
-  pinMode(buttonUnlock, OUTPUT);
-  pinMode(buttonWait, OUTPUT);
-  pinMode(accessRelay, OUTPUT);
+  pinMode(buttonLock, INPUT);
+  pinMode(buttonUnlock, INPUT);
+  pinMode(buttonWait, INPUT);
+  pinMode(accessRelay, INPUT);
   pinMode(doorSensor, INPUT);
-
-  lockServo.attach(servoPin);
-  
-  attachInterrupt(digitalPinToInterrupt(buttonLock), lock, RISING);
-  attachInterrupt(digitalPinToInterrupt(buttonUnlock), unlock, RISING);
-  attachInterrupt(digitalPinToInterrupt(buttonWait), wait, RISING);
-  attachInterrupt(digitalPinToInterrupt(accessRelay), access, FALLING);
   
   Serial.begin(115200);
   Serial.println();
@@ -84,11 +78,20 @@ void setup_wifi()
   WiFi.begin(ssid, password);  //starts wifi connection
 
   while (WiFi.status() != WL_CONNECTED) {  //while wifi not connected...
+    int i;
     Serial.print(".");
     analogWrite(ledPin, 0);  //turn LED off
-    delay(500);   
+    for (i = 0; i <= 50; i++)
+    {
+       buttonPress();
+       delay(10);
+    }
     analogWrite(ledPin, 1023);  //turn LED on
-    delay(500);
+    for (i = 0; i <= 50; i++)
+    {
+       buttonPress();
+       delay(10);
+    }
   }
   Serial.println("");
   Serial.println("WiFi connected");
@@ -160,6 +163,7 @@ void loop()
     reconnect();
   }
   client.loop();
+  buttonPress();
 }
 
 void lockDoor(int lockMode)  //door lock function
@@ -186,43 +190,60 @@ void dimmer()  //dimmer function (for recognizing disconnect when in wall)
   for (i = 0; i < 1023; i++) //loop from dark to bright in ~3.1 seconds
   {
     analogWrite(ledPin, i);  //set PWM rate
+    buttonPress();
     delay(3);  //ideal timing for dimming rate
   }
   for (i = 1023; i > 0; i--) //slowly turn back off
   {
     analogWrite(ledPin, i);
+    buttonPress();
     delay(3);
   }
 }
 
-void lock()
+void buttonPress()  //function that
 {
-  lockDoor(1);
-}
-
-void unlock()
-{
-  lockDoor(0);
-}
-
-void wait()
-{
-  //Wait shit
-  Serial.println("Something's working");
-}
-
-void access()
-{
-  unsigned long currentTime = millis();
-  accessVerify(currentTime);
-}
-
-void accessVerify(unsigned long currentTime)
-{
-  if ((currentTime - lastTime) <= referenceTime)
+  int i;
+  for (i = 0; i < numButtons; i++)  //for loop that checks each button
   {
-    lockDoor(0);
+    int currentButton = atoi(buttonArray[i]);
+    currentStateButton = digitalRead(currentButton);  //current state is reading the state of the button
+    if (currentStateButton)  //if the button is currently being pressed...
+    {
+      switch (currentButton)  //switch statement where the argument is the pin number that is currently being pushed
+      {
+        case buttonLock:  //if it's the lock button...
+          lockDoor(1);  //lock the door
+          client.publish(door_stat, "ON");  //publish that the door's locked
+          delay(20);  //"debounce"
+          break;
+        case buttonUnlock:  //if unlock button..
+          lockDoor(0);
+          client.publish(door_stat, "OFF");
+          delay(20);
+          break;
+        case accessRelay:
+          counter = 0;
+          while (digitalRead(accessRelay))
+          {
+            delay(5);
+            counter += 5;
+          }
+          if ((counter >= (referenceTime - .15 * referenceTime)) || (counter <= (referenceTime + .15 * referenceTime)))
+          {
+            lockDoor(0);
+          }
+          delay(20);
+          break;
+        case buttonWait:
+          //button wait shit here
+          break;
+      }
+      while (digitalRead(currentButton))  //while the current button is still being pressed...
+      {
+        delay(5);  //delay so the esp doesn't blow up/crash :)
+      }
+      delay(20);
+    }
   }
-  lastTime = currentTime;
-  delayMicroseconds(20000);
 }
