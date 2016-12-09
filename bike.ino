2554e20992c465b472c864f93dd6f99e608066f6
papaya96 <oreillyj@purdue.edu>
2016-12-09 09:29:22 -0500

    Update bike.ino

--- bike.ino
+++ bike.ino
@@ -1,11 +1,11 @@
 int leftBlink = 6;
-int rightBlink = 4;
-int headLight = 3;
+int rightBlink = 10;
+int headLight = 9;
 int brakeLight = 2;
-int brakeButton = 5;
-int leftButton = 7;
-int rightButton = 8;
-int headButton = 9;
+int brakeButton = 11;
+int leftButton = 3;
+int rightButton = 5;
+int headButton = 4;
 
 bool rightState = LOW;
 bool leftState = LOW;
@@ -20,6 +20,7 @@ bool headStateB = LOW;
 bool brakeStateB = LOW;
 
 unsigned long int lastTime = millis();
+int resTime = 500;
 
 void headBrake(char*, int);
 
@@ -38,6 +39,9 @@ void setup()
   digitalWrite(leftButton, HIGH);
   digitalWrite(rightButton, HIGH);
   digitalWrite(headButton, HIGH);
+
+  Serial.begin(115200);
+  Serial.println("BEGIN");
 }
 
 void loop()
@@ -47,10 +51,13 @@ void loop()
   headStateB = !digitalRead(headButton);
   brakeStateB = digitalRead(brakeButton);
 
+  //Serial.println(digitalRead(headButton));
+
   if (rightStateB)
   {
+    Serial.println("RIGHT BUTTON");
     leftState = LOW;
-    (rightState)?(rightState = LOW):(rightState = HIGH);
+    (rightState) ? (rightState = LOW) : (rightState = HIGH);
     delay(15);
     while (!digitalRead(rightButton))
     {
@@ -60,8 +67,9 @@ void loop()
   }
   if (leftStateB)
   {
+    Serial.println("LEFT BUTTON");
     rightState = LOW;
-    (leftState)?(leftState = LOW):(leftState = HIGH);
+    (leftState) ? (leftState = LOW) : (leftState = HIGH);
     delay(15);
     while (!digitalRead(leftButton))
     {
@@ -71,13 +79,31 @@ void loop()
   }
   if (headStateB)
   {
+    Serial.println("HEAD BUTTON");
     if (headState)
     {
-      headBrake("h", 0);
+      digitalWrite(headLight, LOW);
+      headState = LOW;
+      Serial.println("HEADLIGHT IS NOW OFF");
+      //headBrake("h", 0);
+      int counter = 0;
+      while (!digitalRead(headButton))
+      {
+        delay(10);
+        counter += 10;
+      }
+      if (counter <= resTime)
+      {
+        Serial.println("ONLY SWITCHED");
+        digitalWrite(headLight, HIGH);
+        headState = HIGH;
+      }
     }
     else
     {
-      headBrake("b", 1);
+      digitalWrite(headLight, HIGH);
+      headState = HIGH;
+      //headBrake("b", 1);
     }
     delay(15);
     while (!digitalRead(headButton))
@@ -85,8 +111,8 @@ void loop()
       delay(50);
     }
   }
-  if (brakeStateB)
-  {
+  /*if (brakeStateB)
+    {
     if (brakeState)
     {
       headBrake("b", 0);
@@ -100,17 +126,19 @@ void loop()
     {
       delay(50);
     }
-  }
+    }*/
   if (rightState)
   {
-    if (rightStateSub && ((millis() - lastTime) >= 1000))
+    if (rightStateSub && ((millis() - lastTime) >= 750))
     {
+      Serial.println("RIGH OFF");
       digitalWrite(rightBlink, LOW);
       rightStateSub = LOW;
       lastTime = millis();
     }
-    else if (!rightStateSub && ((millis() - lastTime) >= 1000))
+    else if (!rightStateSub && ((millis() - lastTime) >= 750))
     {
+      Serial.println("RIGHT ON");
       digitalWrite(rightBlink, HIGH);
       rightStateSub = HIGH;
       lastTime = millis();
@@ -118,18 +146,25 @@ void loop()
   }
   else if (leftState)
   {
-    if (leftStateSub && ((millis() - lastTime) >= 1000))
+    if (leftStateSub && ((millis() - lastTime) >= 750))
     {
+      Serial.println("LEFT OFF");
       digitalWrite(leftBlink, LOW);
       leftStateSub = LOW;
       lastTime = millis();
     }
-    else if (!leftStateSub && ((millis() - lastTime) >= 1000))
+    else if (!leftStateSub && ((millis() - lastTime) >= 750))
     {
+      Serial.println("LEFT ON");
       digitalWrite(leftBlink, HIGH);
       leftStateSub = HIGH;
       lastTime = millis();
     }
+  }
+  else
+  {
+    digitalWrite(leftBlink, HIGH);
+    digitalWrite(rightBlink, HIGH);
   }
 }
 
@@ -147,6 +182,7 @@ void headBrake(char* device, int newState)
       digitalWrite(headLight, LOW);
       headState = LOW;
     }
+    Serial.println("HEAD IS: " + headState);
   }
   else if (device == "b")
   {
